d75ed8ec3536adb7ab3653ed37be65ae02697cf7
HelitonLeal <heliton.leal.84@gmail.com>
2022-08-27 18:04:22 -0300

    Criado context para prover status de logado ou não e com isso redirecionar para páginas disponíveis.

---
+++ src/contexts/auth.js
@@ -0,0 +1,35 @@
+import { useState, useEffect, createContext } from "react";
+import firebase from "../services/firebaseConnection";
+
+export const AuthContext = createContext({});
+
+function AuthProvider({ children }){
+
+    const [user, setUser] = useState(null);
+    const [loadingAuth, setLoadingAuth] = useState(false);
+    const [loading, setLoading] = useState(true);
+
+
+    useEffect(() => {
+
+        function loadStorage(){
+
+            const storageUser = localStorage.getItem('SistemaUser');
+    
+            if(storageUser){
+                setUser(JSON.parse(storageUser));
+                setLoading(false);
+            }
+            setLoading(false);
+        }
+        loadStorage();
+    }, [])
+
+    return(
+        <AuthContext.Provider value={{ signed: !!user, user, loading }}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
+
+export default AuthProvider;
--- src/routes/Route.js
+++ src/routes/Route.js
@@ -1,4 +1,6 @@
+import { useContext } from 'react';
 import { Route, Redirect } from 'react-router-dom';
+import { AuthContext } from '../contexts/auth';
 
 export default function RouteWrapper({
     component: Component,
@@ -6,8 +8,7 @@ export default function RouteWrapper({
     ...rest    
 }){
 
-    const loading = false;
-    const signed = false;
+    const { signed, loading } = useContext(AuthContext);
 
     if(loading){
         return(
